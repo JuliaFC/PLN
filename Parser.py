e011e0235acffb3381fdf8e39568aac377f21dad
JuliaFC <mjfc@cin.ufpe.br>
2021-07-11 20:21:08 -0400

    Refactor entity parser

--- Parser.py
+++ Parser.py
@@ -8,7 +8,9 @@ class Parser:
         self.ents = []
         self.intention = None
         self.questions = []
+        self.wiki_entities = self.load_wiki_entities()
         self.input_filename = input_file
+        self.processed_lines = 0
 
     def trim_whitespace_and_newline(self, answer):
         if answer:
@@ -30,13 +32,37 @@ class Parser:
             self.answers.append(self.format_answers(trimmed_answers))
     
     def parse_entity(self, line):
-        regex = r"\b[A-Z].[\w']+\b(.*)\b[A-Z].[\w']+\b"
-        line = line.split('?')[0]
-        ent = re.search(regex, line)
-        if ent:
-            ent = ent.group()
-            self.ents.append(ent)
+        regex = r"\b(?=\w*[A-Z0-9])\w*\b(.*)([A-Z0-9\'\!]+)\w*"
+        line = line.split('?')[0][1:] # captures the text of the question between '1' and '?'
+        line = self.trim_whitespace_and_newline(line)
+        main_entity = re.search(regex, line)
+        words = line.split(' ')
 
+        entities = []
+        if main_entity:
+            main_entity = main_entity.group()
+            main_entity_words = main_entity.split(' ')
+            for word in words:
+                if word in self.wiki_entities:
+                    entities.append(word)
+                elif word in main_entity_words and main_entity in self.wiki_entities:
+                    entities.append(main_entity)
+                else:
+                    entities.append('0')
+
+            self.ents.append((line, entities))
+    
+    def load_wiki_entities(self):
+        wiki_entities_file = r'C:\Users\JuliaFC\Documents\2021\PLN\PLN\movieqa\knowledge_source\entities.txt'
+        wiki_entities = set()
+        with open(wiki_entities_file, 'r', encoding="utf8") as file:
+            while file:
+                line = file.readline()
+                if not line:
+                    break
+                wiki_entities.add(self.trim_whitespace_and_newline(line))
+        return wiki_entities
+ 
     def parse_intention_from_filename(self):
         regex = r"(?<=_qa_)(.*)(?=_dev)"
         intention = re.search(regex, self.input_filename)
@@ -68,13 +94,15 @@ class Parser:
                 self.parse_entity(line)
                 self.parse_answer(line)
                 self.parse_questions(line)
-
+                self.processed_lines = self.processed_lines + 1
 
     def write_entities(self):
         output_filename = self.create_output_filename("entities")
         with open(output_filename, 'w+', encoding="utf8") as output_file:
-            for (ent, answers) in zip(self.ents, self.answers):
-                output_file.write(f'{ent}\t{answers}\n')
+            for ent in self.ents:
+                question = ent[0]
+                entities = ent[1]
+                output_file.write(f'{question}\t{entities}\n')
 
     def write_intentions(self):
         output_filename = self.create_output_filename("intentions")
